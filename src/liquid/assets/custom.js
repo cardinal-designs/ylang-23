a91283016775beeed6f985d6f9ad6b91d9f45c5b
DevCardinal1 <dev@cardinalco.email>
2023-07-19 12:29:57 +0530

    Solved anniversay page css issue and pushed live theme change to main branch

--- src/liquid/assets/custom.js
+++ src/liquid/assets/custom.js
@@ -7,7 +7,6 @@ customElements.define('custom-style',class customStyle extends HTMLElement {
           styleElement.innerHTML = this.innerHTML;
           this.insertAdjacentElement('afterend',styleElement);
           this.remove();
-          console.log('Custom Style')
       },1000);
     }
   });
